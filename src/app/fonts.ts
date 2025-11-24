f58114884981ac03141eb2534d6818cd4112e5b5
10097-Swarup Hegde <swaruph@linecraft.ai>
2025-11-24 12:22:56 +0530

    added montserrat fonts all across the application

---
+++ src/app/fonts.ts
@@ -0,0 +1,10 @@
+import { Montserrat } from 'next/font/google';
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-montserrat',
+});
+
+export default montserrat;
